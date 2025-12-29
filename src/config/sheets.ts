dd9857d66cb6d3bafe2a3180af5b0933f20da5a6
23891A1231 <mohithgaga82@gmail.com>
2025-12-29 20:14:02 +0530

    Add files via upload

---
+++ src/config/sheets.ts
@@ -0,0 +1,49 @@
+export const SHEET_IDS = {
+  basicInfo: "1aDgEwxudEdFfEMfNkejhR_DPJt1P6pbUGuRjtheWpCY",
+  qualifications: "1hmOn5FP61wSg3ZJhnYfIvs4N23Ba7Glq_yHJBtQ6ESo",
+  teaching: "1ucwLdc_Ufjq0v0DQfD66lqKZF-eytMPgUJbgdNRsY0M",
+  courses: "1ME4ZpDyJEPuIg-D9szfUn3uPETG5EAOHQ43ujnZjL8M",
+  institutional: "1rMWKHbU9diDcSlSs8blIdVoUutEzj7TW4dBcauOR50o",
+  researchProjects: "15hnUjeCU4_mOk-rU4Wl8bqBS0JniudyJCjmMDoe-9gE",
+  journalPublications: "1t6FfGgBrF6pwo1fjDuwEk0x_nGh3e3ULnlP0L9XnZm0",
+  conferencePublications: "1R7ph3n13O45YQPKw68bXLwNWLIqftvvs5-U9kvGd-RE",
+  books: "1UgYjKrYHsCgRUHx2imS1i866sUHRHyKdwE_TAerlUz4",
+
+  patents: "1RqEh6XDnS4ElAWBG4fOlVgPp7JEpAkMcZDwWiMs4uSA",
+  fdp: "136BsDCArdH8wNxit_ejfxJBrljB4ZGV5wqmMjARhhd8",
+
+  memberships: "1S_VqNmSQcsxD99Hl3zVjHaNUvIs2w5-a3fzXm-_RWyY",
+  responsibilities: "1hQlthIu4KMfC-VTOWSFztkMPG2HUSDiQi9h1OcY9qXs",
+  mentoring: "10PwzAtk1_1Q2vLc2ttWgsJ46HPnQ7ujOZMQvGnzX5q4",
+  declaration: "1kSOEVIrVMRcRPmLA6fFCpyhUKxBllAa07jyiKbZDLL0",
+  awards: "1SseYGaGgM7kx1vNBvvKss4pY1nyorDsug-1mcE1IP8o",
+  extension: "1ttZ2G81-gc3cWt0XxH23r-kGMjvymN_LUUyyE3MZ6Z4",
+  onlineProfiles: "1a6qUAEhg4KJLO1iC5VQLJsN284JPFqh_9X8-A5Xc9Co",
+  contactMe: "1utVM7snIBUnudGK3hoz_QZyGMYImQ7BEC1UURyL9mAY",
+};
+
+export const SECTION_TITLES: Record<keyof typeof SHEET_IDS, string> = {
+  basicInfo: "Basic Information",
+  qualifications: "Academic Qualifications",
+  teaching: "Teaching Experience",
+  courses: "Courses Taught",
+  institutional: "Institutional Contributions",
+  journalPublications: "Research & Publications",
+  conferencePublications: "Research & Publications",
+  books: "Research & Publications",
+  researchProjects: "Research & Publications",
+  patents: "Patents & Innovations",
+  fdp: "Development Programs",
+
+  memberships: "Professional Memberships",
+  responsibilities: "Academic Responsibilities",
+  mentoring: "Student Mentoring",
+  declaration: "Declaration",
+  awards: "Awards & Recognitions",
+  extension: "Extension & Outreach",
+
+  onlineProfiles: "Connect",
+  contactMe: "Contact Me",
+};
+
+export const CONTACT_FORM_URL = ""; // PASTE YOUR GOOGLE WEB APP URL HERE
